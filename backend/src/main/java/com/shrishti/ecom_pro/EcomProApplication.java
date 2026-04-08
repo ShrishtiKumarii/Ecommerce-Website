05ccd343d497cd4218bd2ae152631fb64b0669eb
shrishti Kumari <ayushik7899@gmai.com>
2026-04-09 04:12:03 +0530

    Restore full backend structure with controllers, models, services, and initial data

--- backend/src/main/java/com/shrishti/ecom_pro/EcomProApplication.java
+++ backend/src/main/java/com/shrishti/ecom_pro/EcomProApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcomProApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcomProApplication.class, args);
 	}
 
