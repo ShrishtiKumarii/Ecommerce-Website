bd6d14a34725d561bcbefa836c47b29d4d4f112f
shrishti Kumari <ayushik7899@gmai.com>
2026-04-09 01:10:42 +0530

    reconnect project with dockerfile

---
+++ ecom-pro/src/main/java/com/shrishti/ecom_pro/EcomProApplication.java
@@ -0,0 +1,13 @@
+package com.shrishti.ecom_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomProApplication.class, args);
+	}
+
+}
---
+++ ecom-pro/src/test/java/com/shrishti/ecom_pro/EcomProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrishti.ecom_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
